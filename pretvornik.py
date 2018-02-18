c550c2f103f8a1735f4956d00b895a7a0c5d8ed0
chilliandchilli <35531705+chilliandchilli@users.noreply.github.com>
2018-02-18 17:16:02 +0100

    Add files via upload

---
+++ pretvornik.py
@@ -0,0 +1,23 @@
+print "Dobrodosel User3214! Jaz sem pretvornik!"
+
+while True :
+    km = int(raw_input("Vnesi st. km :"))
+    factor = 0.6124
+    milje = km*factor
+    print "Rezultat je", milje, "milje"
+
+
+    choice = raw_input("Zelis pretvoriti se enkrat? (da/ne) :")
+    if choice == "ne" :
+
+        print "Hvala in do naslednjic"
+        break
+
+
+
+
+
+
+
+
+
